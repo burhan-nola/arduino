29b6221eaa087f767105c06189e58506800586f0
= <=>
2024-04-03 16:01:14 +0700

    add readRFID

--- project001/project001.ino
+++ project001/project001.ino
@@ -7,15 +7,13 @@
 
 const char* SSID = "Ruang Guru Lantai 3";
 const char* PASS = "NorthernLight2020";
-
+WiFiServer server(80); 
 String url = "https://habito-api.vercel.app";
 
 PN532_I2C pn532i2c(Wire);
 PN532 nfc(pn532i2c);
 
 String userRFID = "";
-unsigned long previousMillis = 0;
-const long interval = 1000; // Interval antara pembacaan RFID
 
 void setup() {
   Serial.begin(115200);
@@ -39,10 +37,21 @@ void setup() {
   Serial.println("Waiting for an ISO14443A Card ...");
   
   connectWiFi();
+  server.begin();
+  Serial.println("Server started.");
+  //get the ip address and print it
+  Serial.print("This is your ip address: ");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
 }
+static unsigned long lastRFIDPollTime = 0;
+const unsigned long RFIDPollInterval = 10000;
 
 void loop() {
   readRFID();
+//    String uid = "habito_001";
+//    getData(uid);
 
 }
 
@@ -85,15 +94,17 @@ void readRFID(void)
         Serial.println(success);
     
         // Kirim data ke API
-        getData(cardUID);
+        String uid = "habito_001";
+    getData(uid);
         delay(500); // Delay sebelum membaca kartu berikutnya
       }
+//      Serial.println(success);
 }
 
 void getData(String uid){
   WiFiClientSecure client;
   client.setInsecure();
-
+  
   HTTPClient http;
   String fullUrl = "https://habito-api.vercel.app/data?uid="+uid;
   Serial.println(fullUrl);     
@@ -115,5 +126,5 @@ void getData(String uid){
       Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
  }
     http.end();
-    delay(5000);
+//    delay(20000);
 }
